b4ddcf48e6603c07359a63f3b1918c2de73d84ec
KFC <kfc@chen>
2026-07-31 21:37:05 +0000

    refactor: extract ParsedArgs to own file

--- src/Argsparser.cs
+++ src/Argsparser.cs
@@ -14,19 +14,6 @@ namespace Gitic
         }
     }
 
-    public class ParsedArgs
-    {
-        public string Command { get; init; } = string.Empty;
-        public string RepoPath { get; init; } = ".";
-        public AnalysisSettings Settings { get; init; } = new();
-        public string? ContributorName { get; init; }
-        public string? HtmlPath { get; init; }
-        public string? MdPath { get; init; }
-        public string? SvgPath { get; init; }
-        public string? ConfigAction { get; init; }
-        public string? HelpText { get; init; }
-    }
-
     public static class DefaultAnalysisSettings
     {
         public static AnalysisSettings Create() => new()
---
+++ src/ParsedArgs.cs
@@ -0,0 +1,15 @@
+namespace Gitic
+{
+    public class ParsedArgs
+    {
+        public string Command { get; init; } = string.Empty;
+        public string RepoPath { get; init; } = ".";
+        public AnalysisSettings Settings { get; init; } = new();
+        public string? ContributorName { get; init; }
+        public string? HtmlPath { get; init; }
+        public string? MdPath { get; init; }
+        public string? SvgPath { get; init; }
+        public string? ConfigAction { get; init; }
+        public string? HelpText { get; init; }
+    }
+}
