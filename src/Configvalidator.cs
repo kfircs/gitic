6b7a9449e82d3b5af62a6f1b58f35fbaf98eb6c2
KFC <kfc@chen>
2026-07-31 23:44:17 +0000

    refactor: extract DefaultAttentionWeights to own file

--- src/Configvalidator.cs
+++ src/Configvalidator.cs
@@ -6,22 +6,6 @@ using System.Linq;
 
 namespace Gitic
 {
-    public static class DefaultAttentionWeights
-    {
-        public const double Churn = 0.35;
-        public const double Recency = 0.30;
-        public const double ContributorSpread = 0.20;
-        public const double LowFamiliarityConcentration = 0.15;
-
-        public static AttentionWeights Create() => new()
-        {
-            Churn = Churn,
-            Recency = Recency,
-            ContributorSpread = ContributorSpread,
-            LowFamiliarityConcentration = LowFamiliarityConcentration
-        };
-    }
-
     public interface IConfigValidator
     {
         void ValidateOverride(object? input, string source);
---
+++ src/DefaultAttentionWeights.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Gitic
+{
+    public static class DefaultAttentionWeights
+    {
+        public const double Churn = 0.35;
+        public const double Recency = 0.30;
+        public const double ContributorSpread = 0.20;
+        public const double LowFamiliarityConcentration = 0.15;
+
+        public static AttentionWeights Create() => new()
+        {
+            Churn = Churn,
+            Recency = Recency,
+            ContributorSpread = ContributorSpread,
+            LowFamiliarityConcentration = LowFamiliarityConcentration
+        };
+    }
+}
