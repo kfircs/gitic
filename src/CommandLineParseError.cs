47f1c548454378d025a56bae39b435ae135d000a
KFC <kfc@chen>
2026-08-01 20:09:06 +0000

    refactor(cli): conform CommandLineParseError to standard .NET exception guidelines

--- src/CommandLineParseError.cs
+++ src/CommandLineParseError.cs
@@ -4,8 +4,12 @@ namespace Gitic
 {
     public class CommandLineParseError : Exception
     {
+        public CommandLineParseError() : base() { }
+
         public CommandLineParseError(string message) : base(message)
         {
         }
+
+        public CommandLineParseError(string message, Exception innerException) : base(message, innerException) { }
     }
 }
