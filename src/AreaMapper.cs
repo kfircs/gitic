362d7700ef2889d341c791bc41889da1692a92d6
KFC <kfc@chen>
2026-08-01 21:19:52 +0000

    refactor(area-mapper): convert AreaMapper to file-scoped namespace

--- src/AreaMapper.cs
+++ src/AreaMapper.cs
@@ -2,46 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Gitic
+namespace Gitic;
+
+public interface IAreaMapper
 {
-    public interface IAreaMapper
-    {
-        string AreaForPath(
-            string path,
-            int depth,
-            List<NamedArea> namedAreas,
-            HashSet<string>? warnings = null);
-    }
+    string AreaForPath(
+        string path,
+        int depth,
+        List<NamedArea> namedAreas,
+        HashSet<string>? warnings = null);
+}
 
-    public class AreaMapper : IAreaMapper
+public class AreaMapper : IAreaMapper
+{
+    public string AreaForPath(
+        string path,
+        int depth,
+        List<NamedArea> namedAreas,
+        HashSet<string>? warnings = null)
     {
-        public string AreaForPath(
-            string path,
-            int depth,
-            List<NamedArea> namedAreas,
-            HashSet<string>? warnings = null)
+        var matchingAreas = namedAreas.Where(area =>
+            area.Paths.Any(areaPath => PathUtils.MatchesPathPattern(path, areaPath))
+        ).ToList();
+
+        if (matchingAreas.Count > 0)
         {
-            var matchingAreas = namedAreas.Where(area =>
-                area.Paths.Any(areaPath => PathUtils.MatchesPathPattern(path, areaPath))
-            ).ToList();
-
-            if (matchingAreas.Count > 0)
+            if (matchingAreas.Count > 1 && warnings != null)
             {
-                if (matchingAreas.Count > 1 && warnings != null)
-                {
-                    warnings.Add(
-                        $"Path {path} matched multiple configured areas ({string.Join(", ", matchingAreas.Select(a => a.Name))}); using {matchingAreas[0].Name}."
-                    );
-                }
-                return matchingAreas[0].Name;
+                warnings.Add(
+                    $"Path {path} matched multiple configured areas ({string.Join(", ", matchingAreas.Select(a => a.Name))}); using {matchingAreas[0].Name}."
+                );
             }
-
-            var segments = PathUtils.NormalizeGitPath(path).Split('/', StringSplitOptions.RemoveEmptyEntries);
-            if (segments.Length <= 1)
-            {
-                return ".";
-            }
-            return string.Join("/", segments.Take(Math.Min(depth, segments.Length - 1)));
+            return matchingAreas[0].Name;
         }
+
+        var segments = PathUtils.NormalizeGitPath(path).Split('/', StringSplitOptions.RemoveEmptyEntries);
+        if (segments.Length <= 1)
+        {
+            return ".";
+        }
+        return string.Join("/", segments.Take(Math.Min(depth, segments.Length - 1)));
     }
 }
