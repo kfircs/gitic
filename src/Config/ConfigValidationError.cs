842b9f4c44628cd8e930ce918a836d4bd1a3214f
KFC <kfc@chen>
2026-08-04 18:52:15 +0000

    Refactor: ConfigValidationError.cs

--- src/Config/ConfigValidationError.cs
+++ src/Config/ConfigValidationError.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 
 namespace Gitic;
 
+/// <summary>Represents an error that occurs during configuration validation.</summary>
 public sealed class ConfigValidationError : Exception
 {
     public List<string> Details { get; }
 
-    public ConfigValidationError(List<string> details) : base(string.Join(Environment.NewLine, details))
-    {
+    public ConfigValidationError(List<string> details) : base(string.Join(Environment.NewLine, details)) =>
         Details = details;
-    }
 
-    public ConfigValidationError(string message) : base(message) => Details = new List<string> { message };
+    public ConfigValidationError(string message) : base(message) => Details = [message];
 }
 
