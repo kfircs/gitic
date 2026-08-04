990ab89d6a11aa6237f7b0e57492951258e63715
KFC <kfc@chen>
2026-08-04 17:34:54 +0000

    Refactor 14

--- src/Cli/Commands/VersionCommand.cs
+++ src/Cli/Commands/VersionCommand.cs
@@ -9,12 +9,17 @@ public class VersionCommand : ICliCommand
     public Task<CliResult> ExecuteAsync(IConsoleReporter? reporter, CancellationToken cancellationToken = default)
     {
         var assembly = typeof(Cli).Assembly;
-        var version = assembly.GetName().Version?.ToString(3) ?? "0.1.0";
-        var infoVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
-        var displayVersion = string.IsNullOrEmpty(infoVersion) ? version : infoVersion;
+        string displayVersion = GetDisplayVersion(assembly);
 
         string versionText = $"gitic version {displayVersion}\n";
         reporter?.Write(versionText);
         return Task.FromResult(Cli.CliSuccess(versionText));
+    }
+
+    private string GetDisplayVersion(Assembly assembly)
+    {
+        var version = assembly.GetName().Version?.ToString(3) ?? "0.1.0";
+        var infoVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        return string.IsNullOrEmpty(infoVersion) ? version : infoVersion;
     }
 }
