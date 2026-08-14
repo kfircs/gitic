bdaefcab97657fa893ed7ce14ee6a52bc0fe1f08
KFC <kfc@chen>
2026-08-14 14:29:51 +0000

    refact(config): eliminate dynamic reflection in ConfigMerger and delete unused ConfigurationService

--- src/Config/ConfigMerger.cs
+++ src/Config/ConfigMerger.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 namespace Gitic;
 
@@ -22,21 +20,44 @@ public interface IConfigMerger
     GiticConfig MergeMultiple(GiticConfig baseConfig, params object?[] overridesOrConfigs);
 }
 
-public interface IConfigurationService
-{
-    GiticConfig Merge(GiticConfig baseConfig, params object?[] overridesOrConfigs);
-    AnalysisSettings NormalizeSettings(AnalysisSettings? settings);
-    GiticConfigOverrides NormalizeOverrides(object? rawYamlDict, string source);
-}
-
 public class ConfigMerger : IConfigMerger
 {
     public GiticConfig CloneDefaultConfig() => CloneConfig(GiticConfig.Default);
 
     public GiticConfigOverrides ConvertToOverrides(GiticConfig config)
     {
+        return ConvertToOverridesStatic(config);
+    }
+
+    public static GiticConfigOverrides ConvertToOverridesStatic(GiticConfig config)
+    {
         if (config == null) return new GiticConfigOverrides();
-        return MapTo<GiticConfigOverrides>(config);
+
+        return new GiticConfigOverrides
+        {
+            Aliases = config.Aliases,
+            Bots = config.Bots,
+            Excludes = config.Excludes,
+            Areas = config.Areas,
+            Scoring = config.Scoring != null ? new ScoringConfigOverrides
+            {
+                Attention = config.Scoring.Attention != null ? new AttentionWeightsOverrides
+                {
+                    Churn = config.Scoring.Attention.Churn,
+                    Recency = config.Scoring.Attention.Recency,
+                    ContributorSpread = config.Scoring.Attention.ContributorSpread,
+                    LowFamiliarityConcentration = config.Scoring.Attention.LowFamiliarityConcentration
+                } : null
+            } : null,
+            Identity = config.Identity != null ? new IdentityConfigOverrides
+            {
+                MergeOnEmail = config.Identity.MergeOnEmail
+            } : null,
+            Metrics = config.Metrics != null ? new MetricsConfigOverrides
+            {
+                TemporalCouplingMaxCommitFileCount = config.Metrics.TemporalCouplingMaxCommitFileCount
+            } : null
+        };
     }
 
     public GiticConfig CloneConfig(GiticConfig config)
@@ -97,7 +118,7 @@ public class ConfigMerger : IConfigMerger
         var cloned = CloneConfig(baseConfig);
         if (overrideConfig != null)
         {
-            MergeObjects(cloned, overrideConfig);
+            MergeConfigInPlace(cloned, overrideConfig);
         }
         return cloned;
     }
@@ -106,7 +127,7 @@ public class ConfigMerger : IConfigMerger
     public GiticConfig MergeConfig(GiticConfig baseConfig, GiticConfig? overrideConfig)
     {
         if (overrideConfig == null) return CloneConfig(baseConfig);
-        return MergeConfig(baseConfig, ConvertToOverrides(overrideConfig));
+        return MergeConfig(baseConfig, ConvertToOverridesStatic(overrideConfig));
     }
 
     // Deep Interface Implementation: Overload that handles chaining multiple typed overrides
@@ -119,7 +140,7 @@ public class ConfigMerger : IConfigMerger
         {
             if (overrideConfig != null)
             {
-                MergeObjects(cloned, overrideConfig);
+                MergeConfigInPlace(cloned, overrideConfig);
             }
         }
         return cloned;
@@ -137,143 +158,84 @@ public class ConfigMerger : IConfigMerger
 
             if (item is GiticConfig configItem)
             {
-                MergeObjects(cloned, ConvertToOverrides(configItem));
+                MergeConfigInPlace(cloned, ConvertToOverridesStatic(configItem));
             }
             else if (item is GiticConfigOverrides overridesItem)
             {
-                MergeObjects(cloned, overridesItem);
+                MergeConfigInPlace(cloned, overridesItem);
             }
         }
         return cloned;
     }
 
-    private static TTarget MapTo<TTarget>(object source) where TTarget : class, new()
-    {
-        var target = new TTarget();
-        MapProperties(source, target);
-        return target;
-    }
-
-    private static void MapProperties(object source, object target)
-    {
-        if (source == null || target == null) return;
-
-        var sourceProps = source.GetType().GetProperties();
-        var targetProps = target.GetType().GetProperties();
-
-        foreach (var sourceProp in sourceProps)
-        {
-            var targetProp = targetProps.FirstOrDefault(p => p.Name == sourceProp.Name);
-            if (targetProp == null || !targetProp.CanWrite) continue;
-
-            var sourceVal = sourceProp.GetValue(source);
-            if (sourceVal == null) continue;
-
-            // Case 1: Simple assignment if types match (e.g., lists like List<AliasRule>)
-            if (targetProp.PropertyType.IsAssignableFrom(sourceProp.PropertyType))
-            {
-                targetProp.SetValue(target, sourceVal);
-            }
-            // Case 2: Target is Nullable version of Source type (e.g., bool? and bool, double? and double, int? and int)
-            else if (Nullable.GetUnderlyingType(targetProp.PropertyType) == sourceProp.PropertyType)
-            {
-                targetProp.SetValue(target, sourceVal);
-            }
-            // Case 3: Nested objects (e.g., ScoringConfigOverrides and ScoringConfig)
-            else if (targetProp.PropertyType.IsClass && sourceProp.PropertyType.IsClass &&
-                     targetProp.PropertyType != typeof(string) && !typeof(IEnumerable).IsAssignableFrom(targetProp.PropertyType))
-            {
-                var targetVal = targetProp.GetValue(target);
-                if (targetVal == null)
-                {
-                    targetVal = Activator.CreateInstance(targetProp.PropertyType);
-                    targetProp.SetValue(target, targetVal);
-                }
-                if (targetVal != null)
-                {
-                    MapProperties(sourceVal, targetVal);
-                }
-            }
-        }
-    }
-
-    private static void MergeObjects(object target, object source)
+    public static void MergeConfigInPlace(GiticConfig target, GiticConfigOverrides source)
     {
         if (target == null || source == null) return;
 
-        var targetProperties = target.GetType().GetProperties();
-        var sourceProperties = source.GetType().GetProperties();
-
-        foreach (var sourceProp in sourceProperties)
+        if (source.Aliases != null)
         {
-            var val = sourceProp.GetValue(source);
-            if (val == null) continue;
-
-            var targetProp = targetProperties.FirstOrDefault(p => p.Name == sourceProp.Name);
-            if (targetProp == null || !targetProp.CanWrite) continue;
-
-            // Case 1: Collection
-            if (typeof(IList).IsAssignableFrom(targetProp.PropertyType) &&
-                val is IEnumerable sourceCollection)
+            target.Aliases ??= new List<AliasRule>();
+            foreach (var item in source.Aliases)
             {
-                var targetList = targetProp.GetValue(target) as IList;
-                if (targetList == null)
-                {
-                    targetList = Activator.CreateInstance(targetProp.PropertyType) as IList;
-                    targetProp.SetValue(target, targetList);
-                }
-                if (targetList != null)
-                {
-                    foreach (var item in sourceCollection)
-                    {
-                        targetList.Add(item);
-                    }
-                }
-            }
-            // Case 2: Nullable primitive on source, non-nullable primitive on target
-            else if (Nullable.GetUnderlyingType(sourceProp.PropertyType) != null)
-            {
-                targetProp.SetValue(target, val);
-            }
-            // Case 3: Nested object
-            else if (targetProp.PropertyType.IsClass && sourceProp.PropertyType.IsClass &&
-                     targetProp.PropertyType != typeof(string))
-            {
-                var targetVal = targetProp.GetValue(target);
-                if (targetVal != null)
-                {
-                    MergeObjects(targetVal, val);
-                }
+                target.Aliases.Add(item);
             }
         }
-    }
-}
+        if (source.Bots != null)
+        {
+            target.Bots ??= new List<BotRule>();
+            foreach (var item in source.Bots)
+            {
+                target.Bots.Add(item);
+            }
+        }
+        if (source.Excludes != null)
+        {
+            target.Excludes ??= new List<ExcludeRule>();
+            foreach (var item in source.Excludes)
+            {
+                target.Excludes.Add(item);
+            }
+        }
+        if (source.Areas != null)
+        {
+            target.Areas ??= new List<NamedArea>();
+            foreach (var item in source.Areas)
+            {
+                target.Areas.Add(item);
+            }
+        }
 
-public class ConfigurationService : IConfigurationService
-{
-    private readonly IConfigMerger _merger;
-    private readonly IConfigOverridesNormalizer _overridesNormalizer;
-    private readonly IAnalysisSettingsNormalizer _settingsNormalizer;
+        if (source.Scoring != null)
+        {
+            target.Scoring ??= new ScoringConfig();
+            if (source.Scoring.Attention != null)
+            {
+                target.Scoring.Attention ??= new AttentionWeights();
+                var sAttention = source.Scoring.Attention;
+                var tAttention = target.Scoring.Attention;
+                if (sAttention.Churn.HasValue) tAttention.Churn = sAttention.Churn.Value;
+                if (sAttention.Recency.HasValue) tAttention.Recency = sAttention.Recency.Value;
+                if (sAttention.ContributorSpread.HasValue) tAttention.ContributorSpread = sAttention.ContributorSpread.Value;
+                if (sAttention.LowFamiliarityConcentration.HasValue) tAttention.LowFamiliarityConcentration = sAttention.LowFamiliarityConcentration.Value;
+            }
+        }
 
-    public ConfigurationService(IConfigMerger? merger = null, IConfigOverridesNormalizer? overridesNormalizer = null, IAnalysisSettingsNormalizer? settingsNormalizer = null)
-    {
-        _merger = merger ?? new ConfigMerger();
-        _overridesNormalizer = overridesNormalizer ?? new ConfigOverridesNormalizer(new ConfigValidator());
-        _settingsNormalizer = settingsNormalizer ?? new AnalysisSettingsNormalizer();
-    }
+        if (source.Identity != null)
+        {
+            target.Identity ??= new IdentityConfig();
+            if (source.Identity.MergeOnEmail.HasValue)
+            {
+                target.Identity.MergeOnEmail = source.Identity.MergeOnEmail.Value;
+            }
+        }
 
-    public GiticConfig Merge(GiticConfig baseConfig, params object?[] overridesOrConfigs)
-    {
-        return _merger.MergeMultiple(baseConfig, overridesOrConfigs);
-    }
-
-    public AnalysisSettings NormalizeSettings(AnalysisSettings? settings)
-    {
-        return _settingsNormalizer.Normalize(settings ?? new AnalysisSettings());
-    }
-
-    public GiticConfigOverrides NormalizeOverrides(object? rawYamlDict, string source)
-    {
-        return _overridesNormalizer.NormalizeOverride(rawYamlDict, source);
+        if (source.Metrics != null)
+        {
+            target.Metrics ??= new MetricsConfig();
+            if (source.Metrics.TemporalCouplingMaxCommitFileCount.HasValue)
+            {
+                target.Metrics.TemporalCouplingMaxCommitFileCount = source.Metrics.TemporalCouplingMaxCommitFileCount.Value;
+            }
+        }
     }
 }
