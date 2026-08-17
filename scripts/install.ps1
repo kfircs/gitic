35537baee3fb4b5812f3de4f82c218943996a115
GitHub Action <action@github.com>
2026-08-17 18:27:38 +0000

    chore(version): bump version to 0.2.1 [skip ci]

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -12,7 +12,7 @@ Write-Host "Building and packing Gitic..."
 dotnet pack -c Release
 
 $PackagePath = "$ProjectRoot/nupkg"
-$Version = "0.2.0"
+$Version = "0.2.1"
 
 Write-Host "Uninstalling any existing global tool..."
 dotnet tool uninstall -g gitic -ErrorAction SilentlyContinue
